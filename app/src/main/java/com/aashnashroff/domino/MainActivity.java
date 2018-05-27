736d9956a7436c91fabaa28db34d87449b72b44f
Nathan Dalal <nathanhdalal@gmail.com>
2018-05-27 14:38:49 -0700

    basic structure

--- app/src/main/java/com/aashnashroff/domino/MainActivity.java
+++ app/src/main/java/com/aashnashroff/domino/MainActivity.java
@@ -3,11 +3,25 @@ package com.aashnashroff.domino;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.view.View;
+import android.content.Intent;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user touches the play button. */
+    public void startPlay(View view) {
+
+        Intent intent = new Intent(this, PlayActivity.class);
+
+//        intent.putExtra("domino_name", "testDominoName");
+
+        startActivity(intent);
+        setContentView(R.layout.activity_play);
     }
 }
---
+++ app/src/main/java/com/aashnashroff/domino/PlayActivity.java
@@ -0,0 +1,22 @@
+package com.aashnashroff.domino;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class PlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+    }
+
+    public void startLight(View view) {
+
+    }
+
+    public void startGyroscope(View view) {
+
+    }
+}
