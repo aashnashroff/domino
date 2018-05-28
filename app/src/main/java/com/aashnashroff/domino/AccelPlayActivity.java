d4b616aad2126a22ceb60c5ce116fcd6ba554cd0
Aashna Shroff <aashnashroff16@gmail.com>
2018-05-27 17:04:51 -0700

    added page for accel

--- app/src/main/java/com/aashnashroff/domino/AccelPlayActivity.java
+++ app/src/main/java/com/aashnashroff/domino/AccelPlayActivity.java
@@ -1,7 +1,9 @@
 package com.aashnashroff.domino;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AccelPlayActivity extends AppCompatActivity {
 
@@ -10,4 +12,12 @@ public class AccelPlayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accel_play);
     }
+
+    /** Called when the user selects the Accelerometer from the sensor playground. */
+    public void StartAccel(View view) {
+        Intent intent = new Intent(this, PlayActivity.class);
+        startActivity(intent);
+        setContentView(R.layout.activity_accel_play);
+    }
 }
+
