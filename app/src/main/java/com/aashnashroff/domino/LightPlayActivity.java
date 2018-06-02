de02c7a963c35d04beee91e9a387bcb6c3e5b364
Nathan Dalal <nathanhdalal@gmail.com>
2018-06-02 16:53:49 -0700

    decent UI working for light

--- app/src/main/java/com/aashnashroff/domino/LightPlayActivity.java
+++ app/src/main/java/com/aashnashroff/domino/LightPlayActivity.java
@@ -8,73 +8,73 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ProgressBar;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import android.support.v4.graphics.ColorUtils;
+
 public class LightPlayActivity extends AppCompatActivity {
 
-    ProgressBar lightMeter;
-    TextView textMax, textReading;
-    float counter;
-    Button read;
-    TextView display;
+    public static final float BACKGROUND_HUE = 210;
+    public static final float BACKGROUND_SATURATION = 0.5f;
 
+    double luxValue = 0;
+    double maxLux = 50000; /* TODO: change default value here. */
 
+    TextView textReading;
+    LinearLayout layout;
 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        counter = 0;
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_light_play);
 
-        display = (TextView) findViewById(R.id.tvDisplay);
-        lightMeter = (ProgressBar)findViewById(R.id.lightmeter);
-        textMax = (TextView)findViewById(R.id.max);
-        textReading = (TextView)findViewById(R.id.reading);
+        textReading = findViewById(R.id.lux_value);
+        layout = (LinearLayout) findViewById(R.id.play_light_background);
 
-        SensorManager sensorManager
-                = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
-        Sensor lightSensor
-                = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-
-        if (lightSensor == null){
-            Toast.makeText(LightPlayActivity.this,
-                    "Sensor not available.",
-                    Toast.LENGTH_LONG).show();
-        }else{
-            float max =  lightSensor.getMaximumRange();
-            lightMeter.setMax((int)max);
-            textMax.setText("Max Reading(Lux): " + String.valueOf(max));
-
-            sensorManager.registerListener(lightSensorEventListener,
-                    lightSensor,
-                    SensorManager.SENSOR_DELAY_NORMAL);
-
+        SensorManager sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        if (sensorManager != null) {
+            Sensor lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+            if (lightSensor != null) {
+                maxLux = (double) lightSensor.getMaximumRange();
+                sensorManager.registerListener(lightSensorEventListener,
+                        lightSensor,
+                        SensorManager.SENSOR_DELAY_NORMAL);
+                return;
+            }
         }
+
+        Toast.makeText(LightPlayActivity.this,
+                "Sensor not available.",
+                Toast.LENGTH_LONG).show();
     }
 
-    SensorEventListener lightSensorEventListener
-            = new SensorEventListener(){
+    private double luxToEV(double lux) {
+        return Math.log(lux / 2.5) / (Math.log(2));
+    }
 
+    private int getBackgroundColorFromLux(double lux) {
+        double EV = luxToEV(lux);
+        double maxEV = luxToEV(maxLux);
+
+        float brightness = EV > maxEV ? 1 : (float) (EV / maxEV);
+        return ColorUtils.HSLToColor(new float[] {BACKGROUND_HUE, BACKGROUND_SATURATION, brightness});
+    }
+
+    SensorEventListener lightSensorEventListener = new SensorEventListener() {
         @Override
         public void onAccuracyChanged(Sensor sensor, int accuracy) {
-            // TODO Auto-generated method stub
-
+            /* Does nothing. */
         }
 
         @Override
         public void onSensorChanged(SensorEvent event) {
-            // TODO Auto-generated method stub
-            if(event.sensor.getType()==Sensor.TYPE_LIGHT){
-                final float currentReading = event.values[0];
-                lightMeter.setProgress((int)currentReading);
-                textReading.setText("Current Reading(Lux): " + String.valueOf(currentReading));
-
+            if (event.sensor.getType() == Sensor.TYPE_LIGHT) {
+                luxValue = (double) event.values[0];
+                textReading.setText(String.valueOf(luxValue));
+                layout.setBackgroundColor(getBackgroundColorFromLux(luxValue));
             }
         }
 
