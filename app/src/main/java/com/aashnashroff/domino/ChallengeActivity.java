52e1bd7796f9f74991ab2be5ad13e7b012b3e0ab
Aashna Shroff <aashnashroff16@gmail.com>
2018-06-08 23:06:04 -0700

    Added generic challenges page that will work for every sensor. Need to add function to display challenge on build page now.

--- app/src/main/java/com/aashnashroff/domino/ChallengeActivity.java
+++ app/src/main/java/com/aashnashroff/domino/ChallengeActivity.java
@@ -3,7 +3,13 @@ package com.aashnashroff.domino;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Button;
+import android.view.ViewGroup;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ChallengeActivity extends AppCompatActivity {
 
@@ -11,6 +17,37 @@ public class ChallengeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_challenge);
+
+        //get LinearLayout to display buttons on
+        LinearLayout layout = findViewById(R.id.challenges_roadmap);
+
+        Intent intent = getIntent();
+        String[] challenges = intent.getStringArrayExtra("challenges_names");
+        String[] challenges_desc = intent.getStringArrayExtra("challenges_desc");
+
+        //Iterate through challenges. For each challenge, add and display one button.
+        for(int i = 0; i < challenges.length; i++){
+            Button challenge_button = new Button(this);
+            challenge_button.setText(challenges[i]);
+            setOnClick(challenge_button,challenges_desc[i]);
+            challenge_button.setLayoutParams(new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+            layout.addView(challenge_button);
+        }
+    }
+
+
+    public void setOnClick(Button btn, final String challenge_desc){
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChallengeActivity.this, BuildActivity.class);
+                intent.putExtra("challenge_desc", challenge_desc);
+                startActivity(intent);
+                setContentView(R.layout.activity_build);
+            }
+        });
     }
 
     /** Called when the user touches the play button. */
--- app/src/main/java/com/aashnashroff/domino/PlayActivity.java
+++ app/src/main/java/com/aashnashroff/domino/PlayActivity.java
@@ -3,7 +3,9 @@ package com.aashnashroff.domino;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import java.util.Arrays;
 
 public class PlayActivity extends AppCompatActivity {
 
@@ -15,6 +17,15 @@ public class PlayActivity extends AppCompatActivity {
 
     public void startLight(View view) {
         Intent intent = new Intent(this, ChallengeActivity.class);
+
+        //passing sensor information to the Challenges Activity page
+        intent.putExtra("sensor", "light");
+        String[] challenges_names = getResources().getStringArray(R.array.light_challenges_names);
+        Log.d("startLight", "challenge_names: " + Arrays.toString(challenges_names));
+        intent.putExtra("challenges_names", challenges_names);
+        String[] challenges_desc = getResources().getStringArray(R.array.light_challenges_names);
+        intent.putExtra("challenges_desc", challenges_desc);
+
         startActivity(intent);
         setContentView(R.layout.activity_challenge);
     }
