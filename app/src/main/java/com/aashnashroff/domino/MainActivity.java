5e963481537be7e66b41ce2f03ba98759e372fcf
Aashna Shroff <aashnashroff16@gmail.com>
2018-06-15 12:28:15 -0700

    main sensor playground screen added

--- app/src/main/java/com/aashnashroff/domino/MainActivity.java
+++ app/src/main/java/com/aashnashroff/domino/MainActivity.java
@@ -16,9 +16,9 @@ public class MainActivity extends AppCompatActivity {
 
     /** Called when the user touches the play button. */
     public void startPlay(View view) {
-        Intent intent = new Intent(this, PlayActivity.class);
+        Intent intent = new Intent(this, LightInfoActivity.class);
         startActivity(intent);
-        setContentView(R.layout.activity_play);
+        setContentView(R.layout.activity_light_info);
     }
 
     //TODO: Move this function to be called by the appropriate menu button
