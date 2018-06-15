e137de2786a70a5cbc439d02f338a12a14dfae78
Aashna Shroff <aashnashroff16@gmail.com>
2018-06-14 17:17:31 -0700

    some buggy code with new front end

---
+++ app/src/main/java/com/aashnashroff/domino/LightInfoActivity.java
@@ -0,0 +1,33 @@
+package com.aashnashroff.domino;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class LightInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_light_info);
+
+    }
+
+    /** Called when the user touches the back button. */
+    public void onClickBack(View view) {
+        Intent intent = new Intent(this, PlayActivity.class);
+        startActivity(intent);
+        setContentView(R.layout.activity_play);
+    }
+
+    /** Called when the user touches the forward (next) button. */
+    public void onClickForward(View view) {
+        Intent intent = new Intent(this, LightPlayActivity.class);
+        startActivity(intent);
+        setContentView(R.layout.activity_light_play);
+    }
+
+}
+
+
--- app/src/main/java/com/aashnashroff/domino/PlayActivity.java
+++ app/src/main/java/com/aashnashroff/domino/PlayActivity.java
@@ -18,15 +18,18 @@ public class PlayActivity extends AppCompatActivity {
     public void startLight(View view) {
         Intent intent = new Intent(this, ChallengeActivity.class);
 
+        /* This will eventually be used when the front end is not hard coded like it is right now ... sad."
+
         //passing sensor information to the Challenges Activity page
         intent.putExtra("sensor", "light");
         String[] challenges_names = getResources().getStringArray(R.array.light_challenges_names);
         intent.putExtra("challenges_names", challenges_names);
         String[] challenges_desc = getResources().getStringArray(R.array.light_challenges_descriptions);
         intent.putExtra("challenges_desc", challenges_desc);
+        */
 
         startActivity(intent);
-        setContentView(R.layout.activity_challenge);
+        setContentView(R.layout.activity_light_info);
     }
 
     public void startAccelerometer(View view) {
